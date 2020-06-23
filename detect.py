9774664949269276819e86ec3bc46e87f7460d00
Nguyen Xuan Bac <ngxbac.dt@gmail.com>
2020-06-23 21:47:01 +0900

    update lp configs

--- detect.py
+++ detect.py
@@ -96,8 +96,9 @@ def main(_argv):
     image = utils.draw_bbox(original_image, bboxes)
     image = Image.fromarray(image)
     image.show()
-    # image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
-    # cv2.imwrite(FLAGS.output, image)
+    # image.save()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    cv2.imwrite(FLAGS.output, image)
 
 if __name__ == '__main__':
     try:
